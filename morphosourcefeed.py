731050e2ee9016d50459a00399b614263f493ca0
Warren Brown <warrenbrown@ufl.edu>
2018-05-10 09:49:29 -0400

    RC1

--- morphosourcefeed.py
+++ morphosourcefeed.py
@@ -6,53 +6,48 @@ import feedparser, os, sys, urllib, datetime
 #feed_address = "https://www.morphosource.org/rss/ms.rss"
 
 def check_log_file(collection_guid):
-    print("check_log_file")
+    """Checks for log file. If not found, creates one with backdated timestamp."""
     if os.path.isfile('{}.log'.format(collection_guid)):
         if os.access('{}.log'.format(collection_guid), os.R_OK):
             return True
         else:
             exit("Error! Unable to access log file {}.log".format(collection_guid))
     else:
-        back_date = datetime.datetime.now() - datetime.timedelta(days=30)
-        print("check_log_file --> write_pub_date")
+        back_date = datetime.datetime.now() - datetime.timedelta(days=45)
         write_pub_date(collection_guid, back_date)
     return True
 
 def read_pub_date(collection_guid):
-    print("read_pub_date")
     """Returns datetime object of the last pub date for given collection."""
     try:
         with open('{}.log'.format(collection_guid),'r') as f:
             return datetime.datetime.strptime(f.read(), "%Y-%m-%d %H:%M:%S.%f")
     except:
-        sys.exit('Error! Unable to read lastpubdate.log.')
+        sys.exit('Error! Unable to read {}.log.'.format(collection_guid))
 
 def write_pub_date(collection_guid,pub_date):
-    print("write_pub_date")
     """Writes publication date to lastpubdate.log."""
     try:
         with open('{}.log'.format(collection_guid),'w') as f:
             f.write(datetime.datetime.strftime(pub_date, "%Y-%m-%d %H:%M:%S.%f"))
         #f.closed()
     except:
-        sys.exit('Error! Unable to write lastpubdate.log')
+        sys.exit('Error! Unable to write {}.log'.format(collection_guid))
 
 def check_if_new(collection_guid,pub_date):
-    print("check_if_new")
     """Returns True if given pub date is newer than recorded."""
-    print(pub_date, ">", read_pub_date(collection_guid), "?")
     return pub_date > read_pub_date(collection_guid)
 
-def get_new_file(link):
-    print("get_new_file")
+def get_new_file(link, collection_guid, file_path):
     """Downloads the new file."""
-    return urllib.request.urlretrieve(link,"ms.csv")
+    return urllib.request.urlretrieve(link, "{}/{}.csv".format(file_path,collection_guid))
     
 def main(argv):
-    if len(argv) == 3:
+    if len(argv) == 4:
         collection_guid = argv[1]
         if check_log_file(collection_guid):
-            article_title = "MorphoSource media, Audubon Core format for recordset " + collection_guid
+            file_path = argv[3]
+            article_title = "MorphoSource media, Audubon Core format for recordset {}".format(collection_guid)
             feed_address = argv[2]
             article_exists = False
             link = None
@@ -65,14 +60,13 @@ def main(argv):
                 if article_title == entry.title:
                     link = entry.link
                     published_at = datetime.datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z').replace(tzinfo=None)
-                    print(entry.published, "-->", published_at)
                     article_exists = True
 
             if article_exists:
                 if check_if_new(collection_guid, published_at):
-                    # Need to add path handling
-                    get_new_file(link)
+                    get_new_file(link, collection_guid, file_path)
                     write_pub_date(collection_guid, published_at)
+                    print("Update downloaded to {}/{}.csv".format(file_path,collection_guid))
                 else:
                     print("No update.")
             else:
@@ -80,7 +74,7 @@ def main(argv):
 
             # pub_datetime_object = datetime.strptime(feed.entries[0]["published"], '%a, %d %b %Y %H:%M:%S %z')
     else:
-        print("\n\tUsage: python3 morphosourcefeed.py COLLECTIONGUID FEEDADDRESS")
+        print("\n\tUsage: python3 morphosourcefeed.py COLLECTIONGUID FEEDADDRESS /PATH/TO/SAVE/FILE")
     return
 
 if __name__ == "__main__":
