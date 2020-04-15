a415835f22047f81892ca035fddbc9fd21b50ca7
root <root@bacon.floridamuseum.ufl.edu>
2020-04-15 18:18:07 -0400

    adds ability to harvest Specify 7 published Darwin Core archives

--- morphosourcefeed.py
+++ morphosourcefeed.py
@@ -8,7 +8,7 @@
 # https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 
-import feedparser, os, sys, urllib, datetime
+import feedparser, os, sys, urllib, datetime, shutil, zipfile
 
 def check_log_file(collection_guid):
     """Checks for log file. If not found, creates one with backdated timestamp."""
@@ -43,10 +43,24 @@ def check_if_new(collection_guid,pub_date):
     """Returns True if given pub date is newer than recorded."""
     return pub_date > read_pub_date(collection_guid)
 
-def get_new_file(link, collection_guid, file_path):
-    """Downloads the new file."""
+def get_new_morphosource_file(link, collection_guid, file_path):
+    """Downloads new Morphosource file."""
     return urllib.request.urlretrieve(link, "{}/{}.txt".format(file_path,collection_guid))
-    
+
+def get_new_specify_file(link, collection_guid, file_path):
+    '''Downloads new Specify7 dwca and extracts, renames audubon core'''
+    temp_dir = '{}_{}'.format('tmp', collection_guid)
+    os.mkdir(temp_dir)
+    archive, headers = urllib.request.urlretrieve(link, '{}/{}.zip'.format(temp_dir, collection_guid))
+    with zipfile.ZipFile(archive) as archive_zip:
+        with archive_zip.open('multimedia.txt') as audubon_file:
+            with open('{}/{}.txt'.format(file_path, collection_guid), 'w') as outfile:
+                for line in audubon_file:
+                    outfile.write(str(line))
+    shutil.rmtree(temp_dir)
+
+
+
 def main(argv):
     if len(argv) == 4:
         collection_guid = argv[1]
@@ -61,21 +75,26 @@ def main(argv):
             feed_entries = feed.entries
 
             for i,entry in enumerate(feed.entries):
-                #if article_title == entry.title:
-                if "Audubon Core format" in entry.title and collection_guid in entry.title:
+                # Morphosource feed or Specify7 feed
+                morphosource_feed = "Audubon Core format" in entry.title and collection_guid in entry.title
+                specify_feed = 'specify.floridamuseum.ufl.edu' in feed_address and collection_guid in entry.id
+                if morphosource_feed or specify_feed:
                     link = entry.link
                     published_at = datetime.datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z').replace(tzinfo=None)
                     article_exists = True
 
             if article_exists:
                 if check_if_new(collection_guid, published_at):
-                    get_new_file(link, collection_guid, file_path)
+                    if morphosource_feed:
+                        get_new_morphosource_file(link, collection_guid, file_path)
+                    if specify_feed:
+                        get_new_specify_file(link, collection_guid, file_path)
                     write_pub_date(collection_guid, published_at)
                     print("Update downloaded to {}/{}.txt".format(file_path,collection_guid))
                 else:
                     print("No update.")
             else:
-                print("{} does not exist in {}.".format(article_title, feed_address))
+                print("{} does not exist in {}.".format(collection_guid, feed_address))
     else:
         print("\n\tUsage: python morphosourcefeed.py COLLECTIONGUID FEEDADDRESS /PATH/TO/SAVE/FILE")
         print("\n\tRequires Python 3.6.")
